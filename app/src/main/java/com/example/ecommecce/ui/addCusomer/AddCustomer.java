4e873ea9ecbebb9f06066c8f0936e2ed4d622c10
Aliwosabi666 <programmeurweb.a@gmail.com>
2021-04-08 18:53:59 +0100

    first commit

---
+++ app/src/main/java/com/example/ecommecce/ui/addCusomer/AddCustomer.java
@@ -0,0 +1,16 @@
+package com.example.ecommecce.ui.addCusomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ecommecce.R;
+
+public class AddCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_customer);
+    }
+}
---
+++ app/src/main/java/com/example/ecommecce/ui/statut/StatutViewModel.java
@@ -0,0 +1,7 @@
+package com.example.ecommecce.ui.statut;
+
+import androidx.lifecycle.ViewModel;
+
+public class StatutViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
